7820e72560e3ad0bc2d062a9595ff5baeb4b3668
Aleksander Gajos <alek.gajos@gmail.com>
2025-12-08 23:28:22 +0100

    Day 2 part 2 done

--- day2/day2.scala
+++ day2/day2.scala
@@ -14,39 +14,43 @@ def count_digits(x: Long) = {
   digits
 }
 
-def check(range: (Long, Long)): Long = {
+def checkSingleNumber(x: Long, part_two: Boolean): Seq[Long] = {
 
-  // println(range)
+  val len = count_digits(x)
 
-  var count: Long = 0;
+  val range_to_test =
+    if part_two then 1 to len else (len + 1) / 2 to (len + 1) / 2
 
-  val len_a = count_digits(range(0))
-  val len_b = count_digits(range(1))
+  range_to_test flatMap (l => {
+    if (len % l == 0 && l != len) {
+      val pattern = (x % math.pow(10, l)).toLong
+      val rep = (len / l).toInt
+      var whole_number: Long = 0
 
-  if (len_a == len_b && len_a % 2 == 1) { return 0 }
-
-  var number = range(0)
-  var len = count_digits(number)
-  if (len % 2 == 1) {
-      number = math.pow(10, len).toLong
-      len += 1
-  }
-  var first_half = (number / math.pow(10, len / 2)).toLong
-  var whole_number = first_half * math.pow(10, len / 2).toLong + first_half
-
-  while (whole_number <= range(1)) {
-
-
-    if (whole_number <= range(1) && whole_number >= range(0)) {
-      count += whole_number.toLong
-      // println(whole_number)
+      1 to rep foreach { _ =>
+        whole_number = whole_number * math.pow(10, l).toLong + pattern
+      }
+      if (whole_number == x) {
+        Some(whole_number)
+      } else {
+        None
+      }
+    } else {
+      None
     }
+  })
 
-    first_half += 1
-    whole_number = first_half * math.pow(10, count_digits(first_half)).toLong + first_half
+}
+
+def check(range: (Long, Long), part_two: Boolean): Long = {
+
+  val numbers = range(0) to range(1) flatMap { (x: Long) =>
+    val variants =
+      checkSingleNumber(x, part_two).filter(x => x <= range(1) && x >= range(0))
+    if variants.length > 0 then Some(x) else None
   }
 
-  count
+  numbers.sum
 }
 
 class Problem(filePath: String) {
@@ -58,7 +62,6 @@ class Problem(filePath: String) {
         .fromFile(filePath)
         .getLines()
         .map(line => {
-          // println(line)
           line.split(",")
         })
         .next()
@@ -73,8 +76,12 @@ class Problem(filePath: String) {
 
   def solvePart1() = {
     val ranges = parse()
+    ranges.map(check(_, false)).sum
+  }
 
-    ranges.map(check).sum
+  def solvePart2() = {
+    val ranges = parse()
+    ranges.map(check(_, true)).sum
   }
 
 }
@@ -83,8 +90,8 @@ class Problem(filePath: String) {
 
   def process(filePath: String) = {
     println(Problem(filePath).solvePart1())
+    println(Problem(filePath).solvePart2())
   }
 
   process(input)
-  // process(inputFile)
 }
